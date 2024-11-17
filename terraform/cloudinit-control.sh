e9297b5e7227a9494f0336a8c772d576c496f801
Andre Gustavo de C. Albuquerque <gustavo.albuquerque@gmail.com>
2024-11-17 19:57:06 -0300

    Add helm

--- terraform/cloudinit-control.sh
+++ terraform/cloudinit-control.sh
@@ -22,6 +22,7 @@ sudo apt-get install -y jq apparmor-utils vim less apt-transport-https bash-comp
 #KUBEVERSION="v1.31.2"
 #CONTAINERD_VERSION="2.0.0"
 #RUNC_VERSION="v1.2.1"
+#CNI_PLUGIN_VERSION="v1.6.0"
 #CALICO_VERSION="v3.29.0"
 
 # beta: building in ARM support
@@ -46,6 +47,14 @@ EOF
 
 # Apply sysctl params without reboot
 sudo sysctl --system
+
+# update the /etc/hosts to contain the local ip address
+# get the local interface used in the default route
+local_interface=$(ip route show | awk '/default/ {print $5}')
+# get the ip address of the local interface
+local_ip=$(ip addr show ${local_interface} | awk '/inet / {print $2}' | cut -d/ -f1)
+hostname=$(hostname)
+echo -e "\n# k8s cluster hosts\n${local_ip}\t\t${hostname}" | sudo tee -a /etc/hosts
 
 # (Install containerd)
 # getting rid of hard coded version numbers
@@ -78,6 +87,15 @@ fi
 
 wget https://github.com/opencontainers/runc/releases/download/${RUNC_VERSION}/runc.${PLATFORM}
 sudo install -m 755 runc.${PLATFORM} /usr/local/sbin/runc
+
+# Install CNI Plugins
+if [ -z "${CNI_PLUGINS_VERSION}" ]; then
+  CNI_PLUGINS_VERSION=$(curl -s https://api.github.com/repos/containernetworking/plugins/releases/latest | jq -r '.tag_name')
+fi
+wget https://github.com/containernetworking/plugins/releases/download/${CNI_PLUGINS_VERSION}/cni-plugins-linux-${PLATFORM}-${CNI_PLUGINS_VERSION}.tgz
+mkdir -p /opt/cni/bin
+sudo tar xzvf cni-plugins-linux-${PLATFORM}-${CNI_PLUGINS_VERSION}.tgz -C /opt/cni/bin
+
 # Restart containerd
 wget https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
 sudo mv containerd.service /usr/lib/systemd/system/
@@ -101,14 +119,19 @@ cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 br_netfilter
 EOF
 
+# configure kubernetes and helm repos
 curl -fsSL https://pkgs.k8s.io/core:/stable:/${KUBEVERSION}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/${KUBEVERSION}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sleep 2
+curl https://baltocdn.com/helm/signing.asc | sudo gpg --dearmor -o /usr/share/keyrings/helm.gpg
+echo "deb [arch=${PLATFORM} signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 
+# install kube tools
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo swapoff -a
+# install helm
+sudo apt-get install helm
 
 sudo sed -i 's/\/swap/#\/swap/' /etc/fstab
 
